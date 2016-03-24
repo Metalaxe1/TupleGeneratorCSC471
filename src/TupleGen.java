a2dbfba71b238bcb5e5e610bc5726049538db63a
Anthony Ratliff <arratlif@uncg.edu>
2016-03-24 13:23:10 -0400

    Implemented and tested the 'populateArray' method which receives an empty, 2D array and then fills this array to capacity with a randomly generated SSN and a random generated float between 7.50 and 50.00.  The array is then returned full.

--- src/TupleGen.java
+++ src/TupleGen.java
@@ -1,13 +1,33 @@
 
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import java.util.Random;
 
 public class TupleGen{
     public static void main(String[] args) {
-        int numElements = 2000;
+        int numElements = 20;
         String[][] array = populateArray(new String[numElements][2]);
+        for (int i=0;i<numElements;i++){
+            System.out.println(array[i][0] + "\t" + array[i][1]);
+        }
     }
     
     public static String[][] populateArray(String[][] temp){
+        Random rand = new Random();
+        int numberOfItemsAdded = 0;
+        while (numberOfItemsAdded < temp.length){
+            String ssn = randomSSN();
+            boolean contains=false;
+            for (int i=0;i<numberOfItemsAdded;i++){
+                if (temp[i][0].equals(ssn)) contains = true;
+        }
+            if (!contains){
+                temp[numberOfItemsAdded][0] = ssn;
+                DecimalFormat formated = new DecimalFormat("#.00");
+                formated.setRoundingMode(RoundingMode.CEILING);
+                temp[numberOfItemsAdded++][1] = String.valueOf(formated.format((50.00 - 7.50)*rand.nextFloat() + 7.50));
+            }
+        }
         return temp;
     }
     

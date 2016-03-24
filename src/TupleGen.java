fe98ccf74b5210fc72bd7c6a3a2218e3815441a8
Anthony Ratliff <arratlif@uncg.edu>
2016-03-24 12:16:31 -0400

    Created the basic structure and method headers.

--- src/TupleGen.java
+++ src/TupleGen.java
@@ -1,5 +1,19 @@
 public class TupleGen{
     public static void main(String[] args) {
-   
+        int numElements = 2000;
+        String[][] array = populateArray(new String[numElements][2]);
+    }
+    
+    public static String[][] populateArray(String[][] temp){
+        return temp;
+    }
+    
+    public static String randomSSN(){
+        String temp = "";
+        return temp;
+    }
+    
+    public static boolean createFile(String[][] temp){
+        return false;
     }
 }
